6bcab36e37407889d82e5de23ff1e10d5463b0d9
Rebeca C Santos <54561040+us19861229c@users.noreply.github.com>
2020-02-04 18:26:24 -0300

    Atualiza arquivos #2
    Os arquivos atualizados agora contemplam o segundo objetivo do desafio:
    - Campo de filtro para filtrar os membros por Login
    
    O javascript traz as informações de login, que compõem o filtro no HTML, onde se realiza a seleção. 
    É então apresentado  o membro da equipe selecionado, com seu avatar e dado de login. 
    Este procedimento durou 01:03:08

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 //Para buscar através do login, as demais informações sobre cada usuário. Daqui será extraído o login de cada membro
+let tEquipe = [];
 axios.get('https://api.github.com/orgs/grupotesseract/public_members')
     .then(bancoDadosTess => {
         const tesseractGrupo = bancoDadosTess.data;
         tesseractGrupo.forEach( function (dado) {
+            tEquipe.push(dado.data)
             renderUsuario(dado.login)
         })             
     })
@@ -12,8 +14,26 @@ axios.get('https://api.github.com/orgs/grupotesseract/public_members')
 //Para apresentar os membros Foto e Login, numa lista simplificada. A ser exibida no html. 
 async function renderUsuario(usuario) {
     const tdInfo = await axios.get(`https://api.github.com/users/${usuario}`)
-    console.log(tdInfo)
     let elementoLista = document.getElementById("usuario-lista")
+    let elementoItem = document.createElement("li")
+    elementoItem.innerHTML = `<img src="${tdInfo.data.avatar_url}" height="50"><span>${tdInfo.data.login}</span>`
+    elementoLista.appendChild(elementoItem)
+    let seletorDevs = document.getElementById("selecDev")
+    let seletorPadrao = document.createElement("option")
+    seletorPadrao.setAttribute("value", usuario)
+    seletorPadrao.innerHTML += `${usuario}`
+    seletorDevs.appendChild(seletorPadrao)
+}
+
+async function selecionarUsuario() {
+    let elementoLista = document.getElementById("usuario-lista")
+    elementoLista.innerHTML = ""
+    let selecionado = document.getElementById("selecDev").value;
+    console.log(tEquipe)
+    if(selecionado === ""){
+        location.reload();
+    }
+    const tdInfo = await axios.get(`https://api.github.com/users/${selecionado}`)
     let elementoItem = document.createElement("li")
     elementoItem.innerHTML = `<img src="${tdInfo.data.avatar_url}" height="50"><span>${tdInfo.data.login}</span>`
     elementoLista.appendChild(elementoItem)
