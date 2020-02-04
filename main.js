60d7ba75dfec35dedc7622097dbc1953e5004666
Rebeca C Santos <54561040+us19861229c@users.noreply.github.com>
2020-02-04 20:04:43 -0300

    Atualiza arquivos #3
    Implementadas algumas mudanças de nomes(variáveis, funções, ids) e comentários  de modo a melhorar a compreensão do código.
    Uso de Bootstrap na estilização do HTML, para uma apresentação mais amigável dos dados.
    Exibição de mais informações dos usuários (como nome, quantidade de repositórios, quantidade de seguidores e etc)
    
    Este procedimento durou 01:13:44

--- main.js
+++ main.js
@@ -11,30 +11,45 @@ axios.get('https://api.github.com/orgs/grupotesseract/public_members')
     .catch( error => {
         console.log("Desculpe, occoreu um erro e não foi possível obter o resultado esperado.")
     })
-//Para apresentar os membros Foto e Login, numa lista simplificada. A ser exibida no html. 
+//Para apresentar os membros Foto e Login, numa lista simplificada. A ser exibida no HTML. 
 async function renderUsuario(usuario) {
     const tdInfo = await axios.get(`https://api.github.com/users/${usuario}`)
     let elementoLista = document.getElementById("usuario-lista")
     let elementoItem = document.createElement("li")
-    elementoItem.innerHTML = `<img src="${tdInfo.data.avatar_url}" height="50"><span>${tdInfo.data.login}</span>`
+    elementoItem.className ="card m-2 col d-inline-flex border-primary justify-content-around"
+    elementoItem.style = "width: 18rem"
+    elementoItem.innerHTML = `<img src="${tdInfo.data.avatar_url}" class="img-fluid pt-2" height="50">
+                              <h5 class="card-title card-header bg-primary text-white text-center ">${tdInfo.data.name[0]}<span style="font-size:1rem">${tdInfo.data.name.slice(1,)}</span></h5>
+                              <span class="card-text  bg-light"><strong>Repositórios:</strong> ${tdInfo.data.public_repos}</span><br>
+                              <span class="card-text "><strong>Seguidores:</strong> ${tdInfo.data.followers}</span><br>
+                              <span class="card-text  bg-light"><strong>Desde:</strong> ${tdInfo.data.created_at.slice(0,10).split("-").reverse().join("/")}</span><br>
+                              <a href="${tdInfo.data.html_url}" class="mb-1" target="_blank">Conheça mais!</a>`
     elementoLista.appendChild(elementoItem)
-    let seletorDevs = document.getElementById("selecDev")
+    let seletorDevs = document.getElementById("selecDevs")
     let seletorPadrao = document.createElement("option")
     seletorPadrao.setAttribute("value", usuario)
     seletorPadrao.innerHTML += `${usuario}`
     seletorDevs.appendChild(seletorPadrao)
 }
 
+//seleciona o usuário e o apresenta individualmente com mais algumas informações.
 async function selecionarUsuario() {
     let elementoLista = document.getElementById("usuario-lista")
     elementoLista.innerHTML = ""
-    let selecionado = document.getElementById("selecDev").value;
+    let selecionado = document.getElementById("selecDevs").value;
     console.log(tEquipe)
     if(selecionado === ""){
         location.reload();
     }
     const tdInfo = await axios.get(`https://api.github.com/users/${selecionado}`)
     let elementoItem = document.createElement("li")
-    elementoItem.innerHTML = `<img src="${tdInfo.data.avatar_url}" height="50"><span>${tdInfo.data.login}</span>`
+    elementoItem.className ="card m-2 col d-inline-flex border-primary justify-content-around"
+    elementoItem.style = "width: 18rem"
+    elementoItem.innerHTML = `<img src="${tdInfo.data.avatar_url}" class="img-fluid pt-2" height="50">
+                              <h5 class="card-title card-header bg-primary text-white text-center ">${tdInfo.data.name[0]}<span style="font-size:1rem">${tdInfo.data.name.slice(1,)}</span></h5>
+                              <span class="card-text  bg-light"><strong>Repositórios:</strong> ${tdInfo.data.public_repos}</span><br>
+                              <span class="card-text "><strong>Seguidores:</strong> ${tdInfo.data.followers}</span><br>
+                              <span class="card-text  bg-light"><strong>Desde:</strong> ${tdInfo.data.created_at.slice(0,10).split("-").reverse().join("/")}</span><br>
+                              <a href="${tdInfo.data.html_url}" class="mb-1" target="_blank">Conheça mais!</a>`
     elementoLista.appendChild(elementoItem)
 }
